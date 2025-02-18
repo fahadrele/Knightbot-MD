aea713f99e7b4fed58cbeb503016cdae2ec97fb3
fahadrele <fahadraheem88@gmail.com>
2025-02-18 07:26:49 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: 'Fahad',
   botName: "Fadi Bot",
   botOwner: 'F A H A D', // Your name
-  ownerNumber: '+9203146748439', //Your number
+  ownerNumber: '+923146748439', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
