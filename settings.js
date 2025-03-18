3979c86475f4ca660ccd8c4515e8f46670877f4d
fahadrele <fahadraheem88@gmail.com>
2025-03-18 13:51:12 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight',
+  packname: 'Fahad',
   author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botName: "Fadi-Bot",
+  botOwner: 'Fahad', // Your name
+  ownerNumber: '923146748439', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
